8801dec4beb58e8c1a5bbcd948cdd47e364b6cf3
ryotaest <118783403+ryotaest@users.noreply.github.com>
2024-07-16 20:54:54 +1000

    set up cloudinary

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -5,15 +5,22 @@ const campgrounds = require("../controllers/campgrounds");
 const catchAsync = require("../utils/catchAsync");
 const { campgroundSchema } = require("../schemas");
 const { isLoggedIn, isAuthor, validateCampground } = require("../middleware");
+const multer = require("multer");
+const { storage } = require("../cloudinary");
+const upload = multer({ storage });
 
 router
   .route("/")
   .get(catchAsync(campgrounds.index))
-  .post(
-    isLoggedIn,
-    validateCampground,
-    catchAsync(campgrounds.createCampground)
-  );
+  // .post(
+  //   isLoggedIn,
+  //   validateCampground,
+  //   catchAsync(campgrounds.createCampground)
+  // );
+  .post(upload.array("image"), (req, res) => {
+    console.log(req.body, req.files);
+    res.send("itworked?!");
+  });
 router.get("/new", isLoggedIn, campgrounds.renderNewForm);
 router
   .route("/:id")
